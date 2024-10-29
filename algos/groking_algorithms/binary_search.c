826d41a44729ab51b8d0b230bd795378d85e2b2c
root <root@WIN-RV7A35RHII3>
2024-10-29 23:48:19 +0500

    clang-format

--- algos/groking_algorithms/binary_search.c
+++ algos/groking_algorithms/binary_search.c
@@ -1,35 +1,44 @@
 #include <stdio.h>
 #define MASS 10
 
-int binary_search(int* massive, int item, int* counter);
+int binary_search(int *massive, int item, int *counter);
 
 int main() {
-       
-    int massive[MASS] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    int counter = 0;
-    int item, num;
 
-    for (int i = 0; i < MASS; i++) {printf("%d ", massive[i]);}
-    printf("\nEnter number for search: ");
-    scanf("%d", &item);
-    num = binary_search(massive, item, &counter);
-    printf("Position of number: %d; iteration is: %d\n", num, counter);
+  int massive[MASS] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+  int counter = 0;
+  int item, num;
 
-    return 0;
+  for (int i = 0; i < MASS; i++) {
+    printf("%d ", massive[i]);
+  }
+  printf("\nEnter number for search: ");
+  scanf("%d", &item);
+  num = binary_search(massive, item, &counter);
+  printf("Position of number: %d; iteration is: %d\n", num, counter);
+
+  return 0;
 }
 
-int binary_search(int* massive, int item, int* counter) {
-    int low = 0;
-    int top = MASS - 1;
-    while (low <= top) {
-	*counter = *counter + 1;
-        int mid = (low + top) / 2;
-        int guess = massive[mid];
-	printf("Iter: %d, low: %d, top: %d, mid: %d, massive[%d]: %d\n", *counter, low, top, mid, mid, massive[mid]);
-	if (guess == item) { return mid; }
-	if (guess > item) { top = mid - 1; }
-	if (guess < item) { low = mid + 1; }
+int binary_search(int *massive, int item, int *counter) {
+  int low = 0;
+  int top = MASS - 1;
+  while (low <= top) {
+    *counter = *counter + 1;
+    int mid = (low + top) / 2;
+    int guess = massive[mid];
+    printf("Iter: %d, low: %d, top: %d, mid: %d, massive[%d]: %d\n", *counter,
+           low, top, mid, mid, massive[mid]);
+    if (guess == item) {
+      return mid;
     }
-    
-    return -2;
+    if (guess > item) {
+      top = mid - 1;
+    }
+    if (guess < item) {
+      low = mid + 1;
+    }
+  }
+
+  return -1;
 }
